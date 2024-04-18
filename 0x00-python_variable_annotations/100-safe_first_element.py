e5664721c9ac60d6163fc82787a20ce82f313557
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-04-18 22:52:37 +0200

    Py-A-#100

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Annotate parameters
+"""
+
+
+from typing import Sequence, Any, Union, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Augment the following code with the correct duck-typed annotations
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
