227fa9fe367cc7ac15cfa7c9ad650c21759d7b58
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-04-18 21:52:54 +0200

    Py-t-#5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Defined List datType speciified variable
+"""
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """Sum a list of floats
+    """
+    return sum(input_list)
